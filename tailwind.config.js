0c691136ea4094bae94826b474979f384a3016be
kevin-d <kevin.doublet02@gmail.com>
2024-02-21 09:34:04 +0100

    style: add default tailwind colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#a2d492',
+        'secondary': '#3b476c',
+        'tertiary': '#4d96b9',
+        'light': '#e1f1dc',
+      },
+      backgroundColor: {
+        'dark': '#070a06',
+      },
+    },
   },
   plugins: [],
 }
